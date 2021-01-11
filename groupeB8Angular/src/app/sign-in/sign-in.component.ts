b34873cad0dd27b0a408f48772bc0e669cc64325
valentinGailliez <63590566+valentinGailliez@users.noreply.github.com>
2021-01-11 19:14:49 +0100

    Connection Error

--- groupeB8Angular/src/app/sign-in/sign-in.component.ts
+++ groupeB8Angular/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  connected : boolean[] = [true,true];
+  color : string = 'black';
+  form : FormGroup = this.formbuilder.group({
+    email: ['', Validators.required, Validators.email],
+    password: ['', Validators.required]
+  });
+
+  constructor(private formbuilder : FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  connection() {
+    console.log(this.form.value);
+    const password = this.form.value['password'];
+    console.log(password);
+    const email = this.form.value['email'];
+    if(password == "helha"){
+      if(this.form.controls['email'].valid)
+      {
+        alert("Vous êtes connecté");
+      }
+      else this.connected[1] = false;
+    }
+    else{
+      if(this.form.controls['email'].invalid)
+      {
+       this.connected[1] = false;
+      }
+      alert("Vous n'êtes pas connecté");
+      this.getColor();
+      this.connected[0] = false;
+    }
+
+  }
+
+  getColor() {
+    this.color = 'red';
+  }
 }
