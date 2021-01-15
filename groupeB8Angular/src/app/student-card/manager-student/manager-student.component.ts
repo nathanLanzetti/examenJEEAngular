0c20dc4fac1e1819a62dc4b04f0d2623168dd327
valentinGailliez <63590566+valentinGailliez@users.noreply.github.com>
2021-01-15 11:49:42 +0100

    liste

--- groupeB8Angular/src/app/student-card/manager-student/manager-student.component.ts
+++ groupeB8Angular/src/app/student-card/manager-student/manager-student.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {StudentService} from "../../repositories/student.service";
 import {ActivatedRoute} from "@angular/router";
 import {StudentToDB} from "../../models/Student";
