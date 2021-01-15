bca3016184553f9a1b2f217b99aec30119a9089a
valentinGailliez <63590566+valentinGailliez@users.noreply.github.com>
2021-01-15 11:45:32 +0100

    Affichage cours

--- groupeB8Angular/src/app/student-card/manager-student/manager-student.component.ts
+++ groupeB8Angular/src/app/student-card/manager-student/manager-student.component.ts
@@ -11,18 +11,24 @@ import {Subscription} from "rxjs";
 })
 export class ManagerStudentComponent implements OnInit {
   matricule : string;
-  studentDisplay : StudentToDB;
+  student : StudentToDB = null;
 
   private subscriptions: Subscription[] = [];
 
-  constructor(private student: StudentService,
+  constructor(private studentService: StudentService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
       params=>this.matricule=params.get('matricule')
     );
-    this.subscriptions.push(this.student.getByMatricule(this.matricule).subscribe(student=> this.studentDisplay = student));
+    console.log(this.matricule);
+    const sub = this.studentService
+      .getByMatricule(this.matricule)
+      .subscribe(student=> {
+        this.student = student;
+        console.log(this.student)})
+    this.subscriptions.push(sub);
   }
 
   ngOnDestroy(): void {
